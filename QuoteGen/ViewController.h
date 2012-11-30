46ec43f854937754d206cc71105c80be005b2c32
Brian Crider <briancrider@Brians-MacBook-Pro.local>
2012-11-30 16:30:37 -0600

    Initial Commit

---
+++ QuoteGen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuoteGen
+//
+//  Created by Brian Crider on 11/30/12.
+//  Copyright (c) 2012 FedEx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
